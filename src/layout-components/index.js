1c087ff733a2985a5caacb22a6143ea520e458c3
Ahmed48-C <85570965+Ahmed48-C@users.noreply.github.com>
2024-05-26 14:31:14 +0800

    Configured important files and added the header

---
+++ src/layout-components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from './Header';
+export { default as HeaderDots } from './HeaderDots';
+export { default as HeaderDrawer } from './HeaderDrawer';
+export { default as HeaderUserbox } from './HeaderUserbox';
+export { default as HeaderSearch } from './HeaderSearch';
+export { default as HeaderMenu } from './HeaderMenu';
