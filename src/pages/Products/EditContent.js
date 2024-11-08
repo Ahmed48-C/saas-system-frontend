991ca2cc6d3927448b259f525182b2f6de2d2b62
Ahmed48-C <85570965+Ahmed48-C@users.noreply.github.com>
2024-11-08 22:14:14 +0800

    only accept png image for product

--- src/pages/Products/EditContent.js
+++ src/pages/Products/EditContent.js
@@ -35,8 +35,28 @@ const EditContent = ({ editLoading }) => {
         toast.success('Edited Product Successfully');
       };
 
+      // const errorCallback = (error) => {
+      //   toast.error('Error ' + error.message);
+      // };
+
       const errorCallback = (error) => {
-        toast.error('Error ' + error.message);
+        let errorMessage = 'An unexpected error occurred';
+
+        if (error.response) {
+            if (error.response.data && typeof error.response.data === 'object' && error.response.data.detail) {
+                if (error.response.data.detail.includes('PNG')) {
+                  errorMessage = error.response.data.detail;
+                }
+            } else if (error.response.status === 500) {
+                // For server errors
+                errorMessage = 'An error occurred on the server. Please try again later.';
+            }
+        } else if (error.message) {
+            errorMessage = error.message;
+        }
+
+        toast.error('Error: ' + errorMessage);
+        console.log('Detailed error:', error);
       };
 
       handleUpdateRecord(id, updateData, API_ENDPOINTS.PUT_PRODUCT, successCallback, errorCallback);
